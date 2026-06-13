7d5fb73faa233847595729bd5596683d0f52347c
Kumkapalla chidvilas <89988166+chidvilasnaidu@users.noreply.github.com>
2026-06-13 11:50:37 +0530

    Create Groq_simple_Q_A_system.py

---
+++ GEN AI/Practice/Groq_simple_Q_A_system.py
@@ -0,0 +1,57 @@
+from google.colab import userdata
+from groq import Groq
+import os 
+key=userdata.get('Gork_Key')
+os.environ['GROQ_API_KEY']=key
+client=Groq()
+#-----------------------------------------------------------------------------
+#-----------------------------------------------------------------------------
+def invoke_Gork(client, input):
+    chat_completion = client.chat.completions.create(
+      messages=[
+          {
+              "role": "user",
+              "content": f"{input}",
+          }
+      ],
+      model="llama-3.3-70b-versatile",)
+    return chat_completion.choices[0].message.content
+
+#-----------------------------------------------------------------------------
+#-----------------------------------------------------------------------------
+def invoke_Gork_with_prompt(client,input,prompt):
+
+      chat_completion = client.chat.completions.create(
+      messages=[
+          {
+              "role": "user",
+              "content": f"{input}",
+          },
+          {
+              "role": "system",
+              "content": f"{prompt}",
+          }
+      ],
+      model="llama-3.3-70b-versatile",)
+      return chat_completion.choices[0].message.content
+
+
+
+#-----------------------------------------------------------------------------
+#-----------------------------------------------------------------------------
+
+qsn=input(" Enter Question: ")
+if qsn !="":
+  prmpt=input(" Any Prompt Suggestions ...[Yes/No] : ")
+  if prmpt.casefold()=='yes':
+    prompt=input("Enter Suggestions...: ")
+    print(" ")
+    print(invoke_Gork_with_prompt(client, qsn,prompt))
+  elif prmpt.casefold()=='no':
+    print(" ")
+    print(invoke_Gork(client, qsn))
+  else:
+    print(" Please Enter Correct input...! ")
+else:
+  print(" Please Enter Valid question : ")
+
