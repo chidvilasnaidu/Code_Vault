807e49a43569f19374e958ce8d14650ed32291eb
Kumkapalla chidvilas <89988166+chidvilasnaidu@users.noreply.github.com>
2026-06-14 18:36:59 +0530

    Add files via upload

---
+++ GEN AI/Practice/Streamlit_session_behaviour.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.write(st.session_state)
+count=0
+if 'count_var' not in st.session_state:
+     st.session_state['count_var']=0
+else:
+     st.session_state['count_var']+=1
+
+st.write(st.session_state)
